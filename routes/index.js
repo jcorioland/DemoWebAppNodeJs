090742e1e4af31bdd7fe6c3942d5d00f2852800f
Julien Corioland <julien.corioland@live.fr>
2015-10-26 11:30:00 +0100

    update version

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express for DevOps', appVersion: "0.0.3" });
+  res.render('index', { title: 'Express for DevOps', appVersion: "0.0.4" });
 });
 
 module.exports = router;
