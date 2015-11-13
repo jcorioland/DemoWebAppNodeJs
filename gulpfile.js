e6521602ee2a9b4cfec6994499c285c2c2173096
Julien Corioland <julien.corioland@live.fr>
2015-11-13 18:11:09 +0100

    add unit test and packaging

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,14 @@
 var gulp = require('gulp'),
-	mocha = require('gulp-mocha');
+	mocha = require('gulp-mocha'),
+    zip = require('gulp-zip');
 	
 gulp.task('tests', function(){
 	gulp.src('tests/sample.js')
 		.pipe(mocha());
+});
+
+gulp.task('zip', function() {
+    gulp.src('./**/*.*')
+        .pipe(zip('archive.zip'))
+        .pipe(gulp.dest('dist'));
 });
