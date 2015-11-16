53305c613ab5610daa291e3b3d37d481cf793876
Julien Corioland <jucoriol@microsoft.com>
2015-11-16 19:18:12 +0100

    update to version 0.0.3

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express for DevOps', appVersion: "0.0.2" });
+  res.render('index', { title: 'Express for DevOps', appVersion: "0.0.3" });
 });
 
 module.exports = router;
